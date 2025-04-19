2385f389af617f6fe404241b518e2a09f0cd41a5
alikhoshkarhooshsaman <alikhoshkarhooshsaman@gmail.com>
2025-04-19 16:03:35 +0330

    TDD|FX(settings): reset index of settings item if not started from 0

--- classes/Settings.php
+++ classes/Settings.php
@@ -529,6 +529,7 @@ class SAP_Settings {
 				}
 
 				$twitter_keys = $_POST['sap_twitter_options']['twitter_keys'];
+                $twitter_keys = array_values($twitter_keys);
 
 				$twitter_keys_old_data = array();
 				if( !empty( $sap_twitter_options ) ) {
