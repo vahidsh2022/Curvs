d8db5b667b010336595bf9bf225bb889f28b906b
alikhoshkarhooshsaman <alikhoshkarhooshsaman@gmail.com>
2025-04-20 12:42:39 +0330

    TDD(crawlers): add dd

--- classes/Crawlers.php
+++ classes/Crawlers.php
@@ -211,6 +211,7 @@ class SAP_Crawlers
 //        if (sap_get_current_user_role() !== 'superadmin') {
 //            $this->common->redirect('login');
 //        }
+        dd('yes');
         $crawler = $this->getData($this->_id);
         if ($this->send2CrawlerServer($this->_id, $crawler['platform'])) {
             $this->flash->setFlash(lang('crwlr_actv_sucs'), 'success');
